5cbb9793f0528029585acebcf1aa9dc00a22d674
blank <bakareemmanuel98@outlook.com>
2018-04-11 23:22:58 +0100

    Refactored code for caching files

--- materialise-init.js
+++
@@ -1,8 +0,0 @@
-
-function initSelect(){
-    let elem = document.querySelector('select');
-
-    //Would configure options at a later date
-    let options = {}
-    let instance = M.FormSelect.init(elem, options);
-}
--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,40 @@
-// Listen for install event, set callback
-self.addEventListener('install', function(event) {
-      // cache a cat SVG
-  event.waitUntil(
-      initCache()
-  );
+() => {
+    // Listen for install event, set callback
+    self.addEventListener('install', function (event) {
+        // cache a cat SVG
+        if ('caches' in window) {
+            event.waitUntil(
+                //initCache('static-v1')
+            );
+        } else {
+            alert("Cache support is not supported, defaulting to indexedDB");
+        }
+    });
 
-});
+    self.addEventListener('activate', function (event) {
+        event.waitUntil(
+            console.log('Activating')
+            // caches.keys().then(function (cacheNames) {
+            //     return Promise.all(
+            //         cacheNames.filter(function (cacheName) {
+            //             // Return true if you want to remove this cache,
+            //             // but remember that caches are shared across
+            //             // the whole origin
+            //         }).map(function (cacheName) {
+            //             return caches.delete(cacheName);
+            //         })
+            //     );
+            // })
+        );
+    });
 
-self.addEventListener('activate', function(event) {
-    console.log('Service worker is now fully active');
-});
 
-self.addEventListener('fetch', event => {
-    const url = new URL(event.request.url);
-    
-    
-    if (url.origin == location.origin && url.pathname == 'top-headlines') {
-      event.respondWith(caches.match('/countries.json'));
-    }
-  });
+    self.addEventListener('fetch', function(event) {
+        console.log(event.request);
+        event.respondWith(
+          fetch(event.request).catch(function() {
+            return caches.match(event.request);
+          })
+        );
+      });
+}
